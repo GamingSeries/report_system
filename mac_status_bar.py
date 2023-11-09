2798813579fa25c65cdc409303d6594fd19133de
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-11-08 21:39:49 -0500

    mac_status

---
+++ mac_status_bar.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import ttk
+from AppKit import NSApp, NSStatusBar, NSVariableStatusItemLength, NSImageNameInfo
+
+# Your existing code for adding tasks and the GUI goes here
+
+# Create a function to create the macOS status bar application
+def create_mac_status_bar_app():
+    app = NSApp.sharedApplication()
+
+    # Create a status bar item
+    status_item = NSStatusBar.systemStatusBar().statusItemWithLength_(NSVariableStatusItemLength)
+
+    # Set the status item's title and image
+    status_item.setTitle_("Weekly Report")
+    status_item.setImage_(NSImageNameInfo)
+
+    # Add a menu to the status bar item (You can add menu items here)
+    menu = tk.Menu(root)
+    menu.add_command(label="Quit", command=root.quit)  # You can replace "root.quit" with your exit function
+    status_item.setMenu_(menu)
+
+    # Start the main Tkinter loop
+    root.mainloop()
+
+# Call the function to create the macOS status bar application
+if __name__ == "__main__":
+    create_mac_status_bar_app()
